28670e8e63fe76b5185f391a06e369ccdf516d15
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2022-01-09 10:51:02 +0200

    Create factory and seeder for Posts

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        return [
+            'title' => $this->faker->sentence(2),
+            'description' => $this->faker->sentence(),
+            'completed' => rand(0, 1)
+        ];
+    }
+}
--- database/migrations/2022_01_07_093851_create_posts_table.php
+++ database/migrations/2022_01_07_093851_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->longText('description');
+            $table->boolean('completed')->default(0);
             $table->timestamps();
         });
     }
